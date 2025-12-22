bcf1520fa745a769ed9e328fa90fbcc0a510249b
Jenna Deamer <jennadeamer@gmail.com>
2025-12-22 12:30:43 -0500

    basic express setup

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("node:path");
+
+const app = express();
+const PORT = 3000;
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+
+// const messageRouter = require("./routes/messageRouter");
+
+// app.use("/", messageRouter);
+
+app.listen(PORT, () => {
+    console.log(`Express app listening on ${PORT}`);
+});
