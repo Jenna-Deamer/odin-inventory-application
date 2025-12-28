1934b2d53a8e7027c6d9cece5d5bb1a08506c639
Jenna-Deamer <jennadeamer@gmail.com>
2025-12-28 13:24:18 -0500

    Add joins to the select all games query to include Devs and Genres

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,16 @@
 const pool = require("./pool");
 
 async function getAllGames() {
-    const { rows } = await pool.query('SELECT id, title, cover_src FROM games');
+    const { rows } = await pool.query(`SELECT 
+   games.id, games.title, games.cover_src,
+   string_agg(DISTINCT concat(developers.first_name, ' ', developers.last_name), ', ') AS devs,
+   string_agg(DISTINCT genres.title, ', ') AS genres
+FROM games 
+JOIN game_developers ON games.id = game_developers.game_id
+JOIN developers ON game_developers.developer_id = developers.id
+JOIN game_genres ON games.id = game_genres.game_id
+JOIN genres ON game_genres.genre_id = genres.id
+GROUP BY games.id;`);
     console.log(rows)
     return rows;
 };
